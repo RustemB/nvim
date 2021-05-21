a9002cd5ca92305d65d47797d9dd41ecf6aa105f
RustemB <rustemb@systemli.org>
2021-05-21 14:28:28 +0500

    options names are full now and added some lsp keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,9 +1,14 @@
 local keymap = vim.api.nvim_set_keymap
 
+-- code movement
 keymap('n', '<S-Down>', ':m .+1<CR>==', { noremap = true, silent = true })
 keymap('n', '<S-Up>', ':m .-2<CR>==', { noremap = true, silent = true })
 keymap('i', '<S-Down>', '<ESC>:m .+1<CR>==gi', { noremap = true, silent = true })
 keymap('i', '<S-Up>', '<ESC>:m .-2<CR>==gi', { noremap = true, silent = true })
 keymap('v', '<S-Down>', [[:m '>+1<CR>gv=gv]], { noremap = true, silent = true })
 keymap('v', '<S-Up>', [[:m '<-2<CR>gv=gv]], { noremap = true, silent = true })
+
+-- lsp
 keymap('n', 'K', ':lua vim.lsp.buf.hover()<CR>', { silent = true })
+keymap('n', '<F5>', ':lua vim.lsp.buf.rename()<CR>', { silent = true })
+keymap('n', '<leader>gd', ':lua vim.lsp.buf.definition()<CR>', { silent = true })
