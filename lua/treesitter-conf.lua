27a7a102ea44ef288da530d19e5902af8731f8cd
RustemB <rustemb@systemli.org>
2021-05-21 14:59:56 +0500

    added tree-sitter config

---
+++ lua/treesitter-conf.lua
@@ -0,0 +1,10 @@
+require('nvim-treesitter.configs').setup {
+	ensure_installed = {
+		'bash', 'c', 'comment', 'cpp', 'gdscript',
+		'html', 'julia', 'lua', 'python', 'rust',
+		'toml', 'yaml',
+	},
+	highlight = {
+		enable = true,
+	},
+}
