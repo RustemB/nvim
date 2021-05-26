63ba9010cdf7a220b8b137fe814fce64f241df0e
RustemB <rustemb@systemli.org>
2021-05-26 20:43:20 +0500

    added lua lsp

--- lua/lang_serv.lua
+++ lua/lang_serv.lua
@@ -1,4 +1,30 @@
 local nvim_lsp = require('lspconfig')
+local sumneko_root_path = '/usr/share/lua-language-server'
+local sumneko_binary = 'lua-language-server'
+
+nvim_lsp.sumneko_lua.setup {
+  cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" };
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+        path = vim.split(package.path, ';'),
+      },
+      diagnostics = {
+        globals = {'vim'},
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+        },
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
 nvim_lsp.gdscript.setup{}
 nvim_lsp.vimls.setup{}
 nvim_lsp.clangd.setup{}
@@ -6,6 +32,7 @@ nvim_lsp.bashls.setup{}
 nvim_lsp.hls.setup{}
 nvim_lsp.elmls.setup{}
 nvim_lsp.julials.setup{}
+nvim_lsp.yamlls.setup{}
 nvim_lsp.rls.setup {
 	settings = {
 		rust = {
