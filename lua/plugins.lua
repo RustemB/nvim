2dc856abcb147855a7191faff0d59ea8c66ff2bc
RustemB <rustemb@systemli.org>
2021-06-30 23:29:22 +0500

    treesitter-context brokes some things

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -9,7 +9,6 @@ return require('packer').startup(function(use)
     use 'srcery-colors/srcery-vim'
     use 'itchyny/lightline.vim'
     use 'andweeb/presence.nvim'
-    use 'romgrk/nvim-treesitter-context'
     use 'rafcamlet/nvim-luapad'
     use 'npxbr/glow.nvim'
     use {
