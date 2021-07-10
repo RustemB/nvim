bd29c03d9f3f9389f66ffb265b1573ed25745763
RustemB <rustemb@systemli.org>
2021-07-11 03:00:48 +0500

    presence default config was changed

--- lua/discord.lua
+++ lua/discord.lua
@@ -1,9 +1,15 @@
-Presence = require("presence"):setup({
+require("presence"):setup({
     auto_update = true,
-    editing_text = "Editing %s",
-    workspace_text = "Working on %s",
     neovim_image_text = "The One True Text Editor",
     main_image = "neovim",
     log_level = nil,
-    debounce_timeout = 15
+    debounce_timeout = 10,
+    enable_line_number = true,
+    editing_text = "Editing %s",
+    file_explorer_text = "Browsing %s",
+    git_commit_text = "Committing changes",
+    plugin_manager_text = "Managing plugins",
+    reading_text = "Reading %s",
+    workspace_text = "Working on %s",
+    line_number_text = "Line %s out of %s"
 })
