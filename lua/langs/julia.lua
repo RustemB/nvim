42da31f9441aabd69e5ed306a708acbb02e5c3c9
RustemB <rustemb@systemli.org>
2021-07-11 23:06:47 +0500

    julia ft detection

--- lua/langs/julia.lua
+++ lua/langs/julia.lua
@@ -4,4 +4,7 @@ function JuliaDoc(q)
         .system("xdg-open https://docs.julialang.org/en/v1/search/?q=" .. qq)
 end
 
-vim.cmd [[autocmd FileType julia nnoremap <buffer><leader>D :lua JuliaDoc(vim.fn.expand('<cword>'))<CR>]]
+vim.cmd [[
+autocmd FileType julia nnoremap <buffer><leader>D :lua JuliaDoc(vim.fn.expand('<cword>'))<CR>
+autocmd BufEnter *.jl lua vim.opt.filetype = "julia"
+]]
