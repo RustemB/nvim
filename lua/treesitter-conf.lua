9a7bfb21036e07d6571b61109f8d31435242202f
RustemB <rustemb@systemli.org>
2021-07-14 20:13:46 +0500

    OwO

--- lua/treesitter-conf.lua
+++ lua/treesitter-conf.lua
@@ -1,8 +1,8 @@
 require('nvim-treesitter.configs').setup {
     ensure_installed = {
-        "css", "dart", "latex", 'bash', 'c', 'comment', 'cpp', 'fennel',
-        'gdscript', 'haskell', 'html', "javascript", 'julia', 'lua', 'python',
-        "regex", 'rust', 'toml', 'yaml'
+        'css', 'dart', 'latex', 'bash', 'c', 'comment', 'cmake', 'cpp',
+        'fennel', 'gdscript', 'haskell', 'html', 'javascript', 'julia', 'lua',
+        'python', 'regex', 'rust', 'toml', 'yaml'
     },
     highlight = {enable = true}
 }
