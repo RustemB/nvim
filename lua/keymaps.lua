d640e2e82a6cad0ad9c34a1b9abd49a8cae21a17
RustemB <rustemb@systemli.org>
2021-07-18 00:13:06 +0500

    added keymaps (vimL for this moment)

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -18,3 +18,14 @@ vimp.nnoremap(o, '<leader>r', lsp.rename)
 vimp.nnoremap(o, '<leader>gd', lsp.definition)
 vimp.nnoremap(o, 'Q', lsp_code_actions)
 vimp.nnoremap(o, '<leader>ff', lsp.formatting)
+
+vim.cmd [[
+imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
+inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>
+
+snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>
+snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>
+
+imap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
+smap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
+]]
