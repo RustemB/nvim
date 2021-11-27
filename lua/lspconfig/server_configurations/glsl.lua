1a563b7c76991fd0d414401c3c4a553ce63c5ede
RustemB <rustemb@systemli.org>
2021-11-27 17:57:21 +0500

    lol

--- lua/lspconfig/server_configurations/glsl.lua
+++ lua/lspconfig/server_configurations/glsl.lua
@@ -5,10 +5,5 @@ return {
         cmd = {'glslls', '--stdio'},
         filetypes = {'glsl'},
         root_dir = function(filename) return util.path.dirname(filename) end
-    },
-    docs = {
-        description = [[
-https://github.com/bergercookie/asm-lsp
-]]
     }
 }
--- lua/lspconfig/server_configurations/swarm.lua
+++ lua/lspconfig/server_configurations/swarm.lua
@@ -5,12 +5,5 @@ return {
         cmd = {'swarm', 'lsp'},
         filetypes = {'swarm'},
         root_dir = function(filename) return util.path.dirname(filename) end
-    },
-    docs = {
-        package_json = 'https://github.com/facebook/pyre-check/blob/main/tools/ide_plugins/vscode/package.json',
-        description = [[
-https://pyre-check.org/
-`pyre` a static type checker for Python 3.
-]]
     }
 }
